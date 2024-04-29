c76a496296de5d8f8b7481cb5a38c7e76a8fc063
im-sanny <raselparvezsanny@gmail.com>
2024-04-29 09:49:53 +0600

    added update feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,14 +12,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,60 +26,73 @@ async function run() {
     await client.connect();
 
     //database
-    const db = client.db("artsDB")
+    const db = client.db("artsDB");
 
-
-
-    const craftsCollection = db.collection("crafts")
-    const artCollection = db.collection("arts")
+    const craftsCollection = db.collection("crafts");
+    const artCollection = db.collection("arts");
     // crafts
-    app.get('/crafts', async(req, res) =>{
+    app.get("/crafts", async (req, res) => {
       const result = await craftsCollection.find({}).toArray();
       res.send(result);
-    })   
+    });
     // arts
-    app.get('/arts', async(req, res) =>{
+    app.get("/arts", async (req, res) => {
       const result = await artCollection.find({}).toArray();
       res.send(result);
-    })
+    });
 
+    // main section starts from here
+    const addCrafts = db.collection("addCrafts");
 
-// main section starts from here 
-    const addCrafts = db.collection("addCrafts")
-    
-    app.post("/addCraft", async(req, res) =>{
+    app.post("/addCraft", async (req, res) => {
       console.log(req.body);
-      const result = await addCrafts.insertOne(req.body)
+      const result = await addCrafts.insertOne(req.body);
       console.log(result);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-
-    app.get("/myCraft/:email", async(req, res) =>{
+    app.get("/myCraft/:email", async (req, res) => {
       console.log(req.params.email);
-      const result = await addCrafts.find({email: req.params.email}).toArray();
-      res.send(result)
-    })
+      const result = await addCrafts
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
-    app.get("/singleArt/:id", async(req, res) => {
+    app.get("/singleArt/:id", async (req, res) => {
       console.log(req.params.id);
-      const result = await addCrafts.findOne({_id: new ObjectId(req.params.id)})
-      res.send(result)
-    })
+      const result = await addCrafts.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
 
+    app.put("/updateArt/:id", async (req, res) => {
+      console.log(req.params.id);
+      const query = { _id: new ObjectId(req.params.id) };
+      const data = {
+        $set: {
+          name: req.body.name,
+          price: req.body.price,
+          itemName: req.body.itemName,
+        },
+      };
+      const result = await addCrafts.updateOne(query, data);
+      console.log(result);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("art and drawing server is running");
